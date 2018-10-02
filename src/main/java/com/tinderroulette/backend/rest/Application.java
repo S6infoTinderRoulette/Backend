f90aa50e48f6c037943fe3dd6c1efbec2b11dd5e
s0h3ck@gmail.com <s0h3ck@gmail.com>
2018-10-01 20:51:53 -0400

    Test Hello World Controller with Spring Boot

--- src/main/java/com/tinderroulette/backend/rest/Application.java
+++ src/main/java/com/tinderroulette/backend/rest/Application.java
@@ -4,14 +4,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class Application
 {
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+	    return "Hello world";
+    }
+
     public static void main( String[] args )
     {
-    	SpringApplication.run(Application.class);
+    	SpringApplication.run(Application.class, args);
     }
 }
