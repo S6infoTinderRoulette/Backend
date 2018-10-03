7da972cf9c5f9505ad8061de51bc2b2cf35ff788
Projet S6 <chtln.c@hotmail.fr>
2018-10-03 13:31:24 -0400

    CRUD Basique 1.1/2
    Re-push de members

--- src/main/java/com/tinderroulette/backend/rest/controller/MembersController.java
+++ src/main/java/com/tinderroulette/backend/rest/controller/MembersController.java
@@ -1,4 +1,77 @@
 package com.tinderroulette.backend.rest.controller;
 
+import com.tinderroulette.backend.rest.dao.MembersDao;
+import com.tinderroulette.backend.rest.exceptions.EmptyJsonResponse;
+import com.tinderroulette.backend.rest.exceptions.MembersIntrouvableException;
+import com.tinderroulette.backend.rest.model.Members;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
 public class MembersController {
+
+    private MembersDao membersDao;
+
+    public MembersController(MembersDao membersDao) {
+        this.membersDao = membersDao;
+    }
+
+    @GetMapping(value = "/members/{cip}/")
+    public Members findByCip (@PathVariable String cip) {
+        return membersDao.findByCip(cip);
+    }
+
+    @GetMapping (value = "/members/")
+    public List <Members> findAll () {
+        return membersDao.findAll();
+    }
+
+    @PostMapping (value = "/members/member")
+    public ResponseEntity<Void> addMember (@Valid @RequestBody Members members){
+        Members memberstest = membersDao.findByCip(members.getCip());
+        if (memberstest != null) {
+            throw new MembersIntrouvableException("Le membre correspondant au CIP : " + members.getCip() + " est déjà présent dans la base de données");
+        } else {
+            Members memberput = membersDao.save(members);
+            if (memberput == null) {
+                return ResponseEntity.noContent().build();
+            } else {
+                return new ResponseEntity(new EmptyJsonResponse(), HttpStatus.OK);
+            }
+        }
+
+    }
+
+    @PutMapping (value = "/members/member")
+    public ResponseEntity<Void> updateMember (@Valid @RequestBody Members members){
+        Members memberstest = membersDao.findByCip(members.getCip());
+        if (memberstest == null) {
+            throw new MembersIntrouvableException("Le membre correspondant au CIP : " + members.getCip() + " n'est pas présent dans la base de données");
+        } else {
+            Members memberput = membersDao.save(members);
+            if (memberput == null) {
+                return ResponseEntity.noContent().build();
+            } else {
+                return new ResponseEntity(new EmptyJsonResponse(), HttpStatus.OK);
+            }
+        }
+
+    }
+
+    @DeleteMapping (value = "/members/{cip}/")
+    public ResponseEntity <Void> deleteMember (@PathVariable String cip) {
+        Members membertest = membersDao.findByCip(cip);
+        if (membertest == null) {
+            throw new MembersIntrouvableException("Le membre correspondant au CIP : " + cip + " n'est pas présent dans la base de données");
+        } else {
+            membersDao.deleteByCip(cip);
+            return new ResponseEntity(new EmptyJsonResponse(), HttpStatus.OK);
+        }
+    }
+
+
 }
--- src/main/java/com/tinderroulette/backend/rest/dao/MembersDao.java
+++ src/main/java/com/tinderroulette/backend/rest/dao/MembersDao.java
@@ -1,4 +1,20 @@
 package com.tinderroulette.backend.rest.dao;
 
-public class MembersDao {
+import com.tinderroulette.backend.rest.model.Members;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MembersDao extends JpaRepository<Members, String> {
+
+    Members save (Members members);
+    Members findByCip (String cip);
+    List <Members> findByLast_name (String LastName);
+    List <Members> findById_member_status (String idMember);
+    List <Members> findAll (String idMember);
+    void deleteByCip (String cip);
+
+
 }
