18eab79b56de9582b41b7704488a7f82b07c37b4
Projet S6 <chtln.c@hotmail.fr>
2018-10-12 23:09:04 -0400

    Add PartitionneurController - GET Methods

--- src/main/java/com/tinderroulette/backend/rest/controller/PartionneurController.java
+++ src/main/java/com/tinderroulette/backend/rest/controller/PartionneurController.java
@@ -5,12 +5,14 @@ import com.tinderroulette.backend.rest.dao.GroupTypeDao;
 import com.tinderroulette.backend.rest.dao.GroupsDao;
 import com.tinderroulette.backend.rest.dao.MemberClassDao;
 import com.tinderroulette.backend.rest.model.Activities;
+import com.tinderroulette.backend.rest.model.GroupType;
 import com.tinderroulette.backend.rest.model.MemberClass;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 @RestController
 public class PartionneurController {
@@ -34,55 +36,136 @@ public class PartionneurController {
         return EtudiantsActivite;
     }
 
-    @GetMapping (value ="/creationGroupe/{idActivity}/{nbPersonnes}/")
+    @GetMapping (value ="/creationGroupeNb/{idActivity}/{nbPersonnes}/")
     public List<List<MemberClass>> createGroupNbPersonne (@PathVariable int idActivity, @PathVariable int nbPersonnes) {
         Activities activities = activitiesDao.findByIdActivity(idActivity);
         List<MemberClass> EtudiantsActivite = memberClassDao.findByIdClass(activities.getIdClass());
         Collections.shuffle(EtudiantsActivite);
-        System.out.println ("test0");
-        List<List<MemberClass>> Groups = new ArrayList<List<MemberClass>>();
+        List<List<MemberClass>> Groups = new ArrayList<>();
         Iterator<MemberClass> iterator = EtudiantsActivite.iterator();
         while (iterator.hasNext()) {
-            System.out.println ("test1");
             List<MemberClass> tempoList = new ArrayList<>();
             for (int j = 0; j < nbPersonnes; j++) {
                 if(iterator.hasNext()) {
                     tempoList.add(iterator.next());
-                    System.out.println ("test2");
                 } else {
                     break; 
                 }
             }
             Groups.add(tempoList);
-            System.out.println ("test3");
         }
         return Groups;
     }
 
-    @GetMapping (value ="/creationGroupe/{idActivity}/{idGroupType}/")
+    @GetMapping (value ="/creationGroupeGroup/{idActivity}/{idGroupType}/")
     public List<List<MemberClass>> createGroupGroupType (@PathVariable int idActivity, @PathVariable int idGroupType) {
+        GroupType groupType = groupTypeDao.findByIdGroupType(idGroupType);
         Activities activities = activitiesDao.findByIdActivity(idActivity);
         List<MemberClass> EtudiantsActivite = memberClassDao.findByIdClass(activities.getIdClass());
         Collections.shuffle(EtudiantsActivite);
-        System.out.println ("test0");
-        List<List<MemberClass>> Groups = new ArrayList<List<MemberClass>>();
+        List<List<MemberClass>> Groups = new ArrayList<>();
         Iterator<MemberClass> iterator = EtudiantsActivite.iterator();
+
+        int smallestRemainder = Integer.MAX_VALUE;
+        int optimalValue = 0;
+        for (int i = 0; i <= (groupType.getMaxDefault()-groupType.getMinDefault()); i++) {
+            int currentRemainder = (EtudiantsActivite.size() % (groupType.getMinDefault() + i));
+            if (currentRemainder < smallestRemainder || (currentRemainder == smallestRemainder && (groupType.getMinDefault() + i) > optimalValue )) {
+                smallestRemainder = currentRemainder;
+                optimalValue = groupType.getMinDefault() + i;
+            }
+        }
+
         while (iterator.hasNext()) {
-            System.out.println ("test1");
             List<MemberClass> tempoList = new ArrayList<>();
-            for (int j = 0; j < nbPersonnes; j++) {
+            for (int j = 0; j < optimalValue; j++) {
                 if(iterator.hasNext()) {
                     tempoList.add(iterator.next());
-                    System.out.println ("test2");
                 } else {
                     break;
                 }
             }
             Groups.add(tempoList);
-            System.out.println ("test3");
         }
         return Groups;
     }
 
+    @GetMapping (value ="/creationGroupeArrays/{idActivity}/{sizes}/")
+    public List<List<MemberClass>> createGroupArraySizes (@PathVariable int idActivity, @PathVariable int [] sizes) throws Exception{
+        Activities activities = activitiesDao.findByIdActivity(idActivity);
+        List<MemberClass> EtudiantsActivite = memberClassDao.findByIdClass(activities.getIdClass());
+        if (IntStream.of(sizes).sum() > EtudiantsActivite.size()) {
+            throw new Exception("La somme des tailles des groupe est inférieure au nombre d'étudiants inscrits !");
+        } else {
+            Collections.shuffle(EtudiantsActivite);
+            List<List<MemberClass>> Groups = new ArrayList<>();
+            Iterator<MemberClass> iterator = EtudiantsActivite.iterator();
+            while (iterator.hasNext()) {
+                for (int i = 0; i < sizes.length; i++){
+                    List<MemberClass> tempoList = new ArrayList<>();
+                    for (int j = 0; j < sizes[i] ; j++) {
+                        if(iterator.hasNext()) {
+                            tempoList.add(iterator.next());
+                        } else {
+                            break;
+                        }
+                    }
+                    Groups.add(tempoList);
+                }
+            }
+            return Groups;
+        }
+    }
+
+    @GetMapping (value ="/creationGroupeGroupArray/{idActivity}/{idGroupType}/{sizes}/")
+    public List<List<MemberClass>> createGroupGroupTypeAndArray (@PathVariable int idActivity, @PathVariable int idGroupType, @PathVariable int [] sizes) {
+        GroupType groupType = groupTypeDao.findByIdGroupType(idGroupType);
+        Activities activities = activitiesDao.findByIdActivity(idActivity);
+        List<MemberClass> EtudiantsActivite = memberClassDao.findByIdClass(activities.getIdClass());
+        Collections.shuffle(EtudiantsActivite);
+        List<List<MemberClass>> Groups = new ArrayList<>();
+        Iterator<MemberClass> iteratorSizes = EtudiantsActivite.iterator();
+
+        while (iteratorSizes.hasNext()) {
+            for (int i = 0; i < sizes.length; i++){
+                List<MemberClass> tempoList = new ArrayList<>();
+                for (int j = 0; j < sizes[i] ; j++) {
+                    if(iteratorSizes.hasNext()) {
+                        tempoList.add(iteratorSizes.next());
+                    } else {
+                        break;
+                    }
+                }
+                Groups.add(tempoList);
+            }
+        }
+
+        int smallestRemainder = Integer.MAX_VALUE;
+        int optimalValue = 0;
+        if (EtudiantsActivite.size()/groupType.getMinDefault() == 0){
+            optimalValue = EtudiantsActivite.size()-IntStream.of(sizes).sum();
+        } else {
+            for (int i = 0; i <= (groupType.getMaxDefault()-groupType.getMinDefault()); i++) {
+                int currentRemainder = (EtudiantsActivite.size() % (groupType.getMinDefault() + i));
+                if (currentRemainder < smallestRemainder || (currentRemainder == smallestRemainder && (groupType.getMinDefault() + i) > optimalValue )) {
+                    smallestRemainder = currentRemainder;
+                    optimalValue = groupType.getMinDefault() + i;
+                }
+            }
+        }
+
+        while (iteratorSizes.hasNext()) {
+            List<MemberClass> tempoList = new ArrayList<>();
+            for (int j = 0; j < optimalValue; j++) {
+                if(iteratorSizes.hasNext()) {
+                    tempoList.add(iteratorSizes.next());
+                } else {
+                    break;
+                }
+            }
+            Groups.add(tempoList);
+        }
+        return Groups;
+    }
 
 }
