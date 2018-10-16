2cd8cc7585eb177205e8747bfde06d4a248c8c7e
Unknown <victorfan@hotmail.ca>
2018-10-16 12:55:38 -0400

    Replace tabs by spaces

--- src/main/java/com/tinderroulette/backend/rest/controller/MatchMakingController.java
+++ src/main/java/com/tinderroulette/backend/rest/controller/MatchMakingController.java
@@ -17,28 +17,28 @@ import com.tinderroulette.backend.rest.model.MemberClass;
 
 @RestController
 public class MatchMakingController {
-	private MatchMakingDao matchmakingDao;
+    private MatchMakingDao matchmakingDao;
 
-	public MatchMakingController(MatchMakingDao matchmakingDao) {
-		this.matchmakingDao = matchmakingDao;
-	}
+    public MatchMakingController(MatchMakingDao matchmakingDao) {
+        this.matchmakingDao = matchmakingDao;
+    }
 
-	@GetMapping(value = "/matchmaking/members/{idActivity}/")
-	public List<MemberClass> findFreeMembers(@PathVariable int idActivity) {
-		return matchmakingDao.findAllFreeUser(idActivity);
-	}
+    @GetMapping(value = "/matchmaking/members/{idActivity}/")
+    public List<MemberClass> findFreeMembers(@PathVariable int idActivity) {
+        return matchmakingDao.findAllFreeUser(idActivity);
+    }
 
-	@GetMapping(value = "/matchmaking/groups/{idActivity}/{getOpen}/")
-	public List<GroupStudent> findFreeGroup(@PathVariable int idActivity, @PathVariable boolean getOpen) {
-		return getOpen ? matchmakingDao.findAllIncompleteGroups(idActivity)
-				: matchmakingDao.findAllFullGroups(idActivity);
-	}
+    @GetMapping(value = "/matchmaking/groups/{idActivity}/{getOpen}/")
+    public List<GroupStudent> findFreeGroup(@PathVariable int idActivity, @PathVariable boolean getOpen) {
+        return getOpen ? matchmakingDao.findAllIncompleteGroups(idActivity)
+                : matchmakingDao.findAllFullGroups(idActivity);
+    }
 
-	@ResponseBody
-	@PostMapping(value = "/matchmaking/{idActivity}/")
-	public boolean mergeTeam(HttpEntity<String> httpEntity, @PathVariable int idActivity) {
-		JSONObject json = new JSONObject(httpEntity.getBody());
-		String currUser = ConfigurationController.getAuthUser();
-		return matchmakingDao.mergeTeam(json.getString("cip"), currUser, idActivity);
-	}
+    @ResponseBody
+    @PostMapping(value = "/matchmaking/{idActivity}/")
+    public boolean mergeTeam(HttpEntity<String> httpEntity, @PathVariable int idActivity) {
+        JSONObject json = new JSONObject(httpEntity.getBody());
+        String currUser = ConfigurationController.getAuthUser();
+        return matchmakingDao.mergeTeam(json.getString("cip"), currUser, idActivity);
+    }
 }
--- src/main/java/com/tinderroulette/backend/rest/dao/MatchMakingDao.java
+++ src/main/java/com/tinderroulette/backend/rest/dao/MatchMakingDao.java
@@ -14,36 +14,36 @@ import com.tinderroulette.backend.rest.model.MemberClass;
 @Repository
 public class MatchMakingDao {
 
-	@PersistenceContext
-	private EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
-	public List<MemberClass> findAllFreeUser(int idActivity) {
-		String sql = "SELECT * FROM tinderroulette.get_single_member(?)";
-		Query query = em.createNativeQuery(sql, MemberClass.class)
-		        .setParameter(1, idActivity);
-		return (List<MemberClass>) query.getResultList();
-	}
+    public List<MemberClass> findAllFreeUser(int idActivity) {
+        String sql = "SELECT * FROM tinderroulette.get_single_member(?)";
+        Query query = em.createNativeQuery(sql, MemberClass.class)
+                .setParameter(1, idActivity);
+        return (List<MemberClass>) query.getResultList();
+    }
 
-	public List<GroupStudent> findAllIncompleteGroups(int idActivity) {
-		String sql = "SELECT * FROM tinderroulette.get_open_group(?)";
-		Query query = em.createNativeQuery(sql, GroupStudent.class)
-		        .setParameter(1, idActivity);
-		return (List<GroupStudent>) query.getResultList();
-	}
+    public List<GroupStudent> findAllIncompleteGroups(int idActivity) {
+        String sql = "SELECT * FROM tinderroulette.get_open_group(?)";
+        Query query = em.createNativeQuery(sql, GroupStudent.class)
+                .setParameter(1, idActivity);
+        return (List<GroupStudent>) query.getResultList();
+    }
 
-	public List<GroupStudent> findAllFullGroups(int idActivity) {
-		String sql = "SELECT * FROM tinderroulette.get_full_group(?)";
-		Query query = em.createNativeQuery(sql, GroupStudent.class)
-		        .setParameter(1, idActivity);
-		return (List<GroupStudent>) query.getResultList();
-	}
+    public List<GroupStudent> findAllFullGroups(int idActivity) {
+        String sql = "SELECT * FROM tinderroulette.get_full_group(?)";
+        Query query = em.createNativeQuery(sql, GroupStudent.class)
+                .setParameter(1, idActivity);
+        return (List<GroupStudent>) query.getResultList();
+    }
 
-	public boolean mergeTeam(String cip1, String cip2, int idActivity) {
-		String sql = "SELECT * FROM tinderroulette.merge_teams(?,?,?)";
-		Query query = em.createNativeQuery(sql)
-		        .setParameter(1, cip1)
-		        .setParameter(2, cip2)
-		        .setParameter(3, idActivity);
-		return (boolean) query.getSingleResult();
-	}
+    public boolean mergeTeam(String cip1, String cip2, int idActivity) {
+        String sql = "SELECT * FROM tinderroulette.merge_teams(?,?,?)";
+        Query query = em.createNativeQuery(sql)
+                .setParameter(1, cip1)
+                .setParameter(2, cip2)
+                .setParameter(3, idActivity);
+        return (boolean) query.getSingleResult();
+    }
 }
