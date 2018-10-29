6b6f7c35460644fd7c8f3a50513f660e61ad15f7
Marc-Eric Pelletier <medaragon@hotmail.com>
2018-10-29 15:00:37 -0400

    Added the source for the WebConfig class.

--- src/main/java/com/tinderroulette/backend/rest/WebConfig.java
+++ src/main/java/com/tinderroulette/backend/rest/WebConfig.java
@@ -5,6 +5,10 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/*
+* Source (Spring Documentation) : https://spring.io/blog/2015/06/08/cors-support-in-spring-framework#javaconfig
+* */
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
