ba88491b5d688f6bbfa22014d08ff9e4fe77fe69
Unknown <victorfan@hotmail.ca>
2018-11-05 09:58:32 -0500

    Fix put cors and null query

--- src/main/java/com/tinderroulette/backend/rest/dao/SwitchGroupDao.java
+++ src/main/java/com/tinderroulette/backend/rest/dao/SwitchGroupDao.java
@@ -13,15 +13,24 @@ public class SwitchGroupDao {
     private EntityManager em;
 
     public int insertSwitchRequest(String cip, String idClass, int tutorat) {
-        String sql = "SELECT tinderroulette.insert_switchgrouprequest(?, ?, ?)";
-        Query query = em.createNativeQuery(sql).setParameter(1, cip).setParameter(2, idClass).setParameter(3, tutorat);
-        return (int) query.getSingleResult();
+        String sql = "SELECT * FROM tinderroulette.insert_switchgrouprequest(?, ?, ?)";
+        Query query = em.createNativeQuery(sql)
+                .setParameter(1, cip)
+                .setParameter(2, idClass)
+                .setParameter(3, tutorat);
+        Object result = query.getSingleResult();
+        if(result == null) {
+            return 0;
+        }
+        return (int) result;
     }
 
     public boolean acceptSwitchRequest(String cipRequested, String cipUser, String idClass) {
-        String sql = "SELECT tinderroulette.accept_switchgroup( ?, ?, ?);";
-        Query query = em.createNativeQuery(sql).setParameter(1, cipRequested).setParameter(2, cipUser).setParameter(3,
-                idClass);
+        String sql = "SELECT * FROM tinderroulette.accept_switchgroup( ?, ?, ?);";
+        Query query = em.createNativeQuery(sql)
+                .setParameter(1, cipRequested)
+                .setParameter(2, cipUser)
+                .setParameter(3, idClass);
         return (boolean) query.getSingleResult();
     }
 }
