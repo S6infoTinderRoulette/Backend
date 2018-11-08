4e9461ec962860fb5f0991e35310ede2ea9e7d4b
Marc-Eric Pelletier <medaragon@hotmail.com>
2018-11-08 13:20:03 -0500

    Add a few prints know when the data loading is over.

--- src/main/java/com/tinderroulette/backend/rest/DBConnection/ScheduledImport.java
+++ src/main/java/com/tinderroulette/backend/rest/DBConnection/ScheduledImport.java
@@ -17,8 +17,12 @@ public class ScheduledImport {
 
     @Scheduled(cron = "0 0 1 * * *") //every day at 1AM
     public void ScheduledImport() throws IOException {
+        System.out.println("Starting to import APP...");
         dbPopulation.appsPopulation("http://zeus.gel.usherbrooke.ca:8080/ms/rest/app_ap?inscription=2010-01-01");
+        System.out.println("Starting to import members...");
         dbPopulation.membersPopulation("http://zeus.gel.usherbrooke.ca:8080/ms/rest/etudiant_groupe?inscription=2018-01-01&trimestre_id=A18");
+        System.out.println("Starting to import memberclasses...");
         dbPopulation.memberClassPopulation("http://zeus.gel.usherbrooke.ca:8080/ms/rest/etudiant_groupe?inscription=2018-01-01&trimestre_id=A18");
+        System.out.println("Finished importing all the data.");
     }
 }
